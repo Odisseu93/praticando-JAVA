e4852c832961041e9bbad541cbea6530f916e7d2
Ulisses <ulissessuporteti01@gmail.com>
2022-10-21 17:16:38 -0300

    algumas funções para Strings

---
+++ src/AlgumasFuncoesParaStrings.java
@@ -0,0 +1,18 @@
+
+public class AlgumasFuncoesParaStrings {
+
+	public static void main(String[] args) {
+
+		String nome = "Ulisses Silvério";
+		
+		System.out.println(nome);
+		System.out.println("toUpperCase: "+ nome.toUpperCase());
+		System.out.println("toLowerCase: "+ nome.toLowerCase());
+		System.out.println("trim: "+ nome.trim());
+		System.out.println("substring(2): "+ nome.substring(2));
+		System.out.println("substring(2,5): "+ nome.substring(2,5));
+		System.out.println("replace('s','x'): "+ nome.replace('s','x'));
+		
+	}
+
+}
