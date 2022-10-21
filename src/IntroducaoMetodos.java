4fc54cb6489c9cda346bc77053e6a49cb11b5cd9
Ulisses <ulissessuporteti01@gmail.com>
2022-10-21 18:01:14 -0300

    introdução ao metodos de classes

---
+++ src/IntroducaoMetodos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class IntroducaoMetodos {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		System.out.println(fatorial(n));
+
+	}
+
+	/* método fatorial*/
+	public static int fatorial(int n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		} else {
+			return n * fatorial(n - 1);
+		}
+	}
+
+}
